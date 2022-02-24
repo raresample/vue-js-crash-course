6f1f2248834077d28d5a75586890733d9d8ba827
Aaron Hope <aghoecode@gmail.com>
2022-02-23 21:01:55 -0800

    Updated toggleReminder()

--- src/App.vue
+++ src/App.vue
@@ -71,8 +71,20 @@ export default {
         //this.tasks = this.tasks.filter((task) => task.id !== id)
       }
     },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(updTask),
+      })
+
+      const data = await res.json()
+
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task)
     }
   },
   async created() {
